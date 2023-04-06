d324eca9b50a60dfbba8de88310ef940c09d529c
Lena <lena.sterz98@gmail.com>
2023-04-06 10:01:19 +0200

    Add function to show popular tv shows and a loading animation

--- js/script.js
+++ js/script.js
@@ -7,11 +7,15 @@ async function fetchAPIData(endpoint) {
 	const API_KEY = "825997b93aad673e0f714421bcd2b986";
 	const API_URL = "https://api.themoviedb.org/3/";
 
+	showSpinner();
+
 	const response = await fetch(
 		`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
 	);
 
 	const data = await response.json();
+
+	hideSpinner();
 
 	return data;
 }
@@ -42,6 +46,40 @@ async function displayPopularMovies() {
 	});
 }
 
+async function displayPopularShows() {
+	const { results } = await fetchAPIData("tv/popular");
+	results.forEach((show) => {
+		const div = document.createElement("div");
+		div.classList.add("card");
+		div.innerHTML = `
+    <a href="tv-details.html?${show.id}">
+      ${
+				show.poster_path
+					? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}">`
+					: `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}"
+    />`
+			}
+      
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${show.name}</h5>
+      <p class="card-text">
+        <small class="text-muted">Air Date: ${show.first_air_date}</small>
+      </p>
+    </div>`;
+
+		document.querySelector("#popular-shows").appendChild(div);
+	});
+}
+
+function showSpinner() {
+	document.querySelector(".spinner").classList.add("show");
+}
+
+function hideSpinner() {
+	document.querySelector(".spinner").classList.remove("show");
+}
+
 // Highlight active link
 function hightlightActiveLink() {
 	const links = document.querySelectorAll(".nav-link");
@@ -59,7 +97,7 @@ function init() {
 			displayPopularMovies();
 			break;
 		case "/shows.html":
-			console.log("Shows");
+			displayPopularShows();
 			break;
 		case "/movie-details.html":
 			console.log("Movie Details");
